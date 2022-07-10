8edd37809ff7bd5febedc084c7dbd045eb20d521
Anton Illenberger <51234797+GidroN@users.noreply.github.com>
2022-07-10 15:41:57 +0400

    Update main.py

--- main.py
+++ main.py
@@ -16,9 +16,6 @@ TOKEN = os.environ['TOKEN']
 GENIUS_TOKEN = os.environ['GENIUS_TOKEN']
 genius = lyricsgenius.Genius(GENIUS_TOKEN)
 bot = commands.Bot(command_prefix='.', intents=discord.Intents.all())
-#
-# GIULDS = ['Игровой Клуб', 'Real Bot Testing']
-line = []
 FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
 
 
