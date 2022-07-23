8379d25939f64777d8db6bf6d7831524ab706e12
Anton Illenberger <51234797+GidroN@users.noreply.github.com>
2022-07-23 14:18:33 +0400

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ import lyricsgenius
 
 TOKEN = os.environ['TOKEN'] # NEED YOUR DISCORD TOKEN
 GENIUS_TOKEN = os.environ['GENIUS_TOKEN'] # NEED YOUR GENIUS TOKEN
-FFMPEG_PATH = r'' # path to your ffmpeg.exe (for example - C:\Program Files\FFMPEG\bin\ffmpeg.exe)
+FFMPEG_PATH = r'' # (OPTIONAL) path to your ffmpeg.exe (for example - C:\Program Files\FFMPEG\bin\ffmpeg.exe)
 if not TOKEN:
     print('No discord token provided.')
     quit()
