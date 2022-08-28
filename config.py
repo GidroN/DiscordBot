27e8e6aeab0f2e89231267e88bd4f5729d02e5e0
gidron <i@gidroonn.ru>
2022-08-28 10:16:05 +0400

    Added config.py file

---
+++ config.py
@@ -0,0 +1,3 @@
+TOKEN = '' # NEED YOUR DISCORD TOKEN
+GENIUS_TOKEN = '' # NEED YOUR GENIUS TOKEN
+FFMPEG_PATH = r'' # (OPTIONAL) path to your ffmpeg.exe (for example - C:\Program Files\FFMPEG\bin\ffmpeg.exe)
